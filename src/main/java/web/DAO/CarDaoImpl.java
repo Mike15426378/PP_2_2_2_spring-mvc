43cf987faa8822d27ab9a7ae0acd890548984250
Mike15426378 / <Ravaga228>
2022-02-15 21:36:15 +0300

    v2

--- src/main/java/web/DAO/CarDaoImpl.java
+++ src/main/java/web/DAO/CarDaoImpl.java
@@ -11,7 +11,7 @@ public class CarDaoImpl implements CarDao {
         List<Car> cars = new ArrayList<>();
 
         cars.add(new Car("BMW", "X5", "black"));
-        cars.add(new Car("Mercedes", "GL63", "grey"));
+        cars.add(new Car("Mercedes-Benz", "GL63", "grey"));
         cars.add(new Car("Rolls-Royce", "Phantom", "write"));
         cars.add(new Car("AUDI", "RS7", "blue"));
         cars.add(new Car("Ford", "Bronco", "black"));
